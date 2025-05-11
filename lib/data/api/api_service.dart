08bb3fe0c83cc1dd3a6033f9b53ce0b42cc4622a
손진성 <felixson99@gmail.com>
2025-05-11 22:31:49 +0900

    api 파일 조정(api 분류 적용)

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,24 +1,68 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:movie_information_app/data/model/movie.dart';
 
 class MovieApi {
   final String apiKey = 'f8f541812e1aae2feeaab5cae1eb9c93';
   final String baseUrl = 'https://api.themoviedb.org/3';
      static const String readAccessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOGY1NDE4MTJlMWFhZTJmZWVhYWI1Y2FlMWViOWM5MyIsIm5iZiI6MTc0Njk2NTQwMC44NTYsInN1YiI6IjY4MjA5Mzk4OTE5ZDkzYWZlNjg2M2NmNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EJgBJYNAY_LPiNMw58_rjpDgJ1EvOJeQD39fb6bIoIk';
 
-
-  Future<List<Movie>> fetchPopularMovies() async {
-    final url = Uri.parse('$baseUrl/movie/popular?api_key=$apiKey');
-    final response = await http.get(url);
+  Future<List<dynamic>> fetchPopularMovies() async {
+    final response = await http.get(
+      Uri.parse(
+        '$baseUrl/discover/movie?api_key=$apiKey&language=en-US&page=1&sort_by=popularity.desc'
+      ),
+    );
 
     if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      List movies = data['results'];
-
-      return movies.map((movie) => Movie.fromJson(movie)).toList();
+      Map<String, dynamic> data = json.decode(response.body);
+      return data['results'];
     } else {
-      throw Exception('Failed to load movies');
+      throw Exception('영화 불러오기 실패');
+    }
+  }
+
+  Future<List<dynamic>> fetchTopRatedMovies() async {
+    final response = await http.get(
+      Uri.parse(
+        '$baseUrl/discover/movie?api_key=$apiKey&language=en-US&page=1&sort_by=vote_average.desc'
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      return data['results'];
+    } else {
+      throw Exception('영화 불러오기 실패');
+    }
+  }
+
+  Future<List<dynamic>> fetchUpcomingMovies() async {
+    final response = await http.get(
+      Uri.parse(
+        '$baseUrl/movie/upcoming?api_key=$apiKey&language=en-US&page=1'
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      return data['results'];
+    } else {
+      throw Exception('영화 불러오기 실패');
+    }
+  }
+
+  Future<List<dynamic>> fetchNowPlayingMovies() async {
+    final response = await http.get(
+      Uri.parse(
+        '$baseUrl/movie/now_playing?api_key=$apiKey&language=en-US&page=1'
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      return data['results'];
+    } else {
+      throw Exception('영화 불러오기 실패');
     }
   }
 }
--- lib/data/model/movie.dart
+++ lib/data/model/movie.dart
@@ -1,14 +1,14 @@
 class Movie {
   final String title;
-  final String poster;
+  final String posterPath;
   final double rating;
 
-  Movie({required this.title, required this.poster, required this.rating});
+  Movie({required this.title, required this.posterPath, required this.rating});
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
     title: json['title'] ?? '제목없음', 
-    poster: json['poster'] ?? '',
+    posterPath: json['poster'] ?? '',
     rating: json['vote_average']?.toDouble()?? 0.0);
   }
 }
