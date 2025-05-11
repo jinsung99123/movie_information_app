e359dff1183277e98e485d8d5ed3c7188c224160
손진성 <felixson99@gmail.com>
2025-05-11 20:02:00 +0900

    UI 기초 예시 뼈대 작성

--- lib/UI/detail/detail_page.dart
+++ lib/UI/detail/detail_page.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class DetailPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Text('이미지'),
+          Text('제목/날짜'),
+          Text('소제목'),
+          Text('키워드'),
+          Text('영화 설명')
+        ],
+      ),
+    );
   }
 } 
--- lib/UI/home/home_page.dart
+++ lib/UI/home/home_page.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Text('가장 인기 있는'),
+          Text('현재 상영중'),
+          Text('인기순'),
+          Text('평점 높은순'),
+          Text('개봉예정정')
+
+        ],
+      ),
+    );
   }
 } 
