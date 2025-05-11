3bcfe988591541fb167283f856212e111069ca4e
손진성 <felixson99@gmail.com>
2025-05-11 20:26:53 +0900

    위젯 기초 리스트뷰 설정

--- lib/UI/home/widgets/home_ing.dart
+++ lib/UI/home/widgets/home_ing.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 class HomeIng extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+        height: 200,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Text('1'),
+            Text('2'),
+            Text('3'),
+            Text('4')
+          ],
+        ),
+      );
   }
 }
