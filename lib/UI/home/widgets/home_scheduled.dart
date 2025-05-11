ce6d60025dcae0a05afa392d0dca93ba5ba1a6fc
손진성 <felixson99@gmail.com>
2025-05-11 20:39:52 +0900

    리스트 제목 설정

--- lib/UI/home/widgets/home_scheduled.dart
+++ lib/UI/home/widgets/home_scheduled.dart
@@ -3,30 +3,38 @@ import 'package:flutter/material.dart';
 class HomeScheduled extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 200,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: [
-            Container(
-              width: 150,
-              color: Colors.red,
-              child: Text('1'),
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 50,
+          child: Text('개봉예정')),
+        Container(
+            height: 200,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  width: 150,
+                  color: Colors.red,
+                  child: Text('1'),
+                ),
+                Container(
+                  width: 150,
+                  color: Colors.amber,
+                  child: Text('2')),
+                Container(
+                  width: 150,
+                  color: Colors.green,
+                  child: Text('3')),
+                Container(
+                  width: 150,
+                  color: Colors.blue,
+                  child: Text('4'))
+              ],
             ),
-            Container(
-              width: 150,
-              color: Colors.amber,
-              child: Text('2')),
-            Container(
-              width: 150,
-              color: Colors.green,
-              child: Text('3')),
-            Container(
-              width: 150,
-              color: Colors.blue,
-              child: Text('4'))
-          ],
-        ),
-      );
+          ),
+      ],
+    );
   }
 }
