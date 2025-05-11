6a9c69801cdc3b415aff42bd85150d07bdefa92a
손진성 <felixson99@gmail.com>
2025-05-11 21:59:41 +0900

    api 요청 파일 수정

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,26 +1,24 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../model/movie.dart';
+import 'package:movie_information_app/data/model/movie.dart';
 
-class ApiService {
-  static const String baseUrl = 'https://api.themoviedb.org/3';
-  static const String apiKey = 'f8f541812e1aae2feeaab5cae1eb9c93';
-  static const String readAccessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOGY1NDE4MTJlMWFhZTJmZWVhYWI1Y2FlMWViOWM5MyIsIm5iZiI6MTc0Njk2NTQwMC44NTYsInN1YiI6IjY4MjA5Mzk4OTE5ZDkzYWZlNjg2M2NmNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EJgBJYNAY_LPiNMw58_rjpDgJ1EvOJeQD39fb6bIoIk';
+class MovieApi {
+  final String apiKey = 'f8f541812e1aae2feeaab5cae1eb9c93';
+  final String baseUrl = 'https://api.themoviedb.org/3';
+     static const String readAccessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOGY1NDE4MTJlMWFhZTJmZWVhYWI1Y2FlMWViOWM5MyIsIm5iZiI6MTc0Njk2NTQwMC44NTYsInN1YiI6IjY4MjA5Mzk4OTE5ZDkzYWZlNjg2M2NmNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EJgBJYNAY_LPiNMw58_rjpDgJ1EvOJeQD39fb6bIoIk';
 
-  static Future<List<Movie>> fetchPopularMovies() async {
-    final response = await http.get(
-      Uri.parse('$baseUrl/movie/popular?api_key=$apiKey&language=ko-KR'),
-      headers: {
-        'Authorization': 'Bearer $readAccessToken',
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    );
+
+  Future<List<Movie>> fetchPopularMovies() async {
+    final url = Uri.parse('$baseUrl/movie/popular?api_key=$apiKey');
+    final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      final List results = jsonDecode(response.body)['results'];
-      return results.map((json) => Movie.fromJson(json)).toList();
+      final data = jsonDecode(response.body);
+      List movies = data['results'];
+
+      return movies.map((movie) => Movie.fromJson(movie)).toList();
     } else {
-      throw Exception('영화 로드 실패');
+      throw Exception('Failed to load movies');
     }
   }
 }
--- lib/data/model/movie.dart
+++ lib/data/model/movie.dart
@@ -1,12 +1,14 @@
 class Movie {
   final String title;
   final String poster;
+  final double rating;
 
-  Movie({required this.title, required this.poster});
+  Movie({required this.title, required this.poster, required this.rating});
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
     title: json['title'] ?? '제목없음', 
-    poster: json['poster'] ?? '');
+    poster: json['poster'] ?? '',
+    rating: json['vote_average']?.toDouble()?? 0.0);
   }
 }
