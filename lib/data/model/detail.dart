12de813f673af791cb57d5d32e61f81af8424719
손진성 <felixson99@gmail.com>
2025-05-12 21:26:15 +0900

    detail model 타입 오류 수정

--- lib/data/model/detail.dart
+++ lib/data/model/detail.dart
@@ -3,14 +3,14 @@ class MovieDetail {
   final String releaseDate;
   final String tagline;
   final String runtime;
-  final String genres;
+  final List<String> genres;
   final String overview;
   final String voteAverage;
   final String voteCount;
   final String popularity;
   final String budget;
   final String revenue;
-  final String productionCompanies;
+  final List<String> productionCompanies;
 
   MovieDetail({
     required this.title,
@@ -33,14 +33,14 @@ class MovieDetail {
       releaseDate: json['release_date'] ?? '',
       tagline: json['tagline'] ?? '',
       runtime: json['runtime'] ?? 0,
-      genres: json['genres'] ?? [],
+      genres: List<String>.from(json['genres'] ?? []),
       overview: json['overview'] ?? '',
       voteAverage: (json['vote_average'] ?? 0).toDouble(),
       voteCount: json['vote_count'] ?? 0,
       popularity: (json['popularity'] ?? 0).toDouble(),
       budget: json['budget'] ?? 0,
       revenue: json['revenue'] ?? 0,
-      productionCompanies: json['production_companies'] ?? [],
+      productionCompanies: List<String>.from(json['production_companies']?.map((e) => e['name'] ?? '') ?? []),
     );
   }
 }
