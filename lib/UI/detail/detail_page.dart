b1340735aee4c9d71097e0c0529808f3914b6fb7
손진성 <felixson99@gmail.com>
2025-05-12 21:24:58 +0900

    detail page UI 뷰모델과 연결

--- lib/UI/detail/detail_page.dart
+++ lib/UI/detail/detail_page.dart
@@ -10,65 +10,70 @@ class DetailPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final detailState = ref.watch(detailViewModelProvider(movie.id));
+    final detailState = ref.watch(detailViewModelProvider(movie.id)); // movie.id를 전달
 
     return Scaffold(
       appBar: AppBar(title: Text('상세 페이지')),
-      body: ListView(
-        children: [
-          Container(height: 500,
-          child: Image.network('http://tmdb.org/t/p/w500${movie.posterPath}'),),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'moana 2',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                    Text('2024-11-27'),
-                  ],
-                ),
-                Text('the ocean is calling them back'),
-                Text('100분'),
-                Text('태그 태그 태그 태그'),
-                Text('영화 설명'),
-                SizedBox(
-                  height: 60,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      Text('평점'),
-                      Text('평점투표수'),
-                      Text('인기점수'),
-                      Text('예산'),
-                      Text('수익')
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 60,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      Text('제작사1'),
-                      Text('제작사2'),
-                      Text('제작사3')
-                    ],
-                  ),
-                ),
-                
-              ],
+      body: detailState.when(
+        loading: () => Center(child: CircularProgressIndicator()),
+        data: (movieDetail) => ListView(
+          children: [
+            Container(
+              height: 500,
+              child: Image.network('http://tmdb.org/t/p/w500${movie.posterPath}'),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        movieDetail.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(movieDetail.releaseDate),
+                    ],
+                  ),
+                  Text(movieDetail.tagline),
+                  Text('${movieDetail.runtime}분'),
+                  Text(movieDetail.genres),
+                  Text(movieDetail.overview),
+                  SizedBox(
+                    height: 60,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Text('평점: ${movieDetail.voteAverage}'),
+                        Text('평점 투표수: ${movieDetail.voteCount}'),
+                        Text('인기 점수: ${movieDetail.popularity}'),
+                        Text('예산: ${movieDetail.budget}'),
+                        Text('수익: ${movieDetail.revenue}'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 60,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: movieDetail.productionCompanies
+                          .map((company) => Text(company))
+                          .toList(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        error: (error, stackTrace) => Center(
+          child: Text('영화 정보를 가져오는 데 실패했습니다.'),
+        ),
       ),
     );
   }
